216672e71c2eac44b9c2c6b36df177cc6b80cc79
Roderick MacCrimmon <r.maccrimm@gmail.com>
2025-11-11 15:19:51 -0700

    Lexer working

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ enum Token {
     Extern,
     Identifier { name: String },
     Number { value: f64 },
-    Other { ascii_value: u32 },
+    Other(char),
 }
 
 struct Lexer {
@@ -34,19 +34,22 @@ impl Lexer {
     }
 
     fn get_char(&mut self) -> Option<char> {
+        self.pos += 1;
         if self.pos == self.input.len() {
             return None;
         }
-        let c = self.input[self.pos];
-        self.pos += 1;
-        Some(c)
+        Some(self.input[self.pos])
     }
 
     fn get_token(&mut self) -> Result<Token> {
-        let mut last_char = ' ';
-        while last_char.is_whitespace() {
+        if self.pos == self.input.len() {
+            return Ok(Token::EndOfFile);
+        }
+
+        let mut current = self.input[self.pos];
+        while current.is_whitespace() {
             let next = self.get_char();
-            last_char = match next {
+            current = match next {
                 Some(c) => c,
                 None => {
                     return Ok(Token::EndOfFile);
@@ -54,24 +57,55 @@ impl Lexer {
             }
         }
 
-        if last_char.is_alphabetic() {
+        if current.is_alphabetic() {
             let mut ident = String::new();
-            while last_char.is_alphabetic() {
-                ident += &last_char.to_string();
-                last_char = match self.get_char() {
+            while current.is_alphabetic() {
+                ident += &current.to_string();
+                current = match self.get_char() {
                     Some(c) => c,
                     None => {
                         break;
                     }
                 }
             }
+            if &ident == "extern" {
+                return Ok(Token::Extern);
+            }
+            if &ident == "def" {
+                return Ok(Token::Def);
+            }
             return Ok(Token::Identifier { name: ident });
         }
-        Err(anyhow!("Failed to get token"))
+
+        if current.is_ascii_digit() {
+            let mut x = String::new();
+            while current.is_ascii_digit() || current == '.' {
+                x += &current.to_string();
+                current = match self.get_char() {
+                    Some(c) => c,
+                    None => {
+                        break;
+                    }
+                }
+            }
+            let value: f64 = x.parse()?;
+            return Ok(Token::Number { value });
+        }
+
+        self.pos += 1;
+        Ok(Token::Other(current))
     }
 }
 
 fn main() -> Result<()> {
+    let mut lexer = Lexer::from_stdin()?;
+    loop {
+        let t = lexer.get_token()?;
+        println!("{:#?}", t);
+        if t == Token::EndOfFile {
+            break;
+        }
+    }
     Ok(())
 }
 
@@ -80,7 +114,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_token_identifiers() {
+    fn test_get_token() {
         assert_eq!(
             Lexer::from_str("a").get_token().unwrap(),
             Token::Identifier { name: "a".into() }
@@ -94,5 +128,15 @@ mod tests {
             }
         );
         assert_eq!(Lexer::from_str(" ").get_token().unwrap(), Token::EndOfFile);
+        assert_eq!(
+            Lexer::from_str("1").get_token().unwrap(),
+            Token::Number { value: 1.0 }
+        );
+        assert_eq!(
+            Lexer::from_str("1.234").get_token().unwrap(),
+            Token::Number { value: 1.234 }
+        );
+
+        assert!(Lexer::from_str("1.234.34").get_token().is_err());
     }
 }
