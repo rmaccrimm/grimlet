4696c16c45e86ed6f013505394dae6c59601a6ab
Roderick MacCrimmon <r.maccrimm@gmail.com>
2025-11-21 22:39:43 -0700

    Work on Branch implementation

--- src/jit/branch.rs
+++ src/jit/branch.rs
@@ -16,6 +16,7 @@ impl<'ctx> Compiler<'ctx> {
                 panic!("Bad operand")
             }
         };
+        // TODO - backwards jumps?
         match self.get_compiled_func_pointer(FuncCacheKey(target as u32))? {
             Some(func_ptr) => {
                 // Jump directly to compiled function
@@ -30,7 +31,20 @@ impl<'ctx> Compiler<'ctx> {
             }
             None => {
                 // Context switch and jump out to the interpreter
-                self.context_switch_out(func.state_ptr, func.regs_ptr);
+                self.context_switch_out(&func)?;
+                let func_ptr = self.get_external_func_pointer(ArmState::jump_to as u64)?;
+                let call = bd.build_indirect_call(
+                    self.void_t
+                        .fn_type(&[self.ptr_t.into(), self.i32_t.into()], false),
+                    func_ptr,
+                    &[
+                        func.state_ptr.into(),
+                        self.i32_t.const_int(target as u64, false).into(),
+                    ],
+                    "call",
+                )?;
+                call.set_tail_call(true);
+                bd.build_return(None)?;
             }
         };
         Ok(())
