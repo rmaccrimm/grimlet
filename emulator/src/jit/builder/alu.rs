a9c29ac92b4f8ef0e18157537ad6bbb5daa24f33
Roderick MacCrimmon <r.maccrimm@gmail.com>
2025-12-05 17:59:09 -0700

    Implement MUL, SUB, and MOV simplest cases

--- emulator/src/jit/builder/alu.rs
+++ emulator/src/jit/builder/alu.rs
@@ -1,48 +1,101 @@
 use crate::{
     arm::{cpu::Reg, disasm::ArmDisasm},
-    jit::{FunctionBuilder, builder::InstrHist},
+    jit::FunctionBuilder,
 };
 use anyhow::{Result, anyhow};
-use capstone::arch::arm::ArmOperandType;
+use capstone::{RegId, arch::arm::ArmOperandType};
 
 impl<'ctx, 'a> FunctionBuilder<'ctx, 'a> {
     pub(super) fn arm_cmp(&mut self, instr: &ArmDisasm) {
         // Essential a NOP, until the flags are actually used
         let reg = self.reg_map.get(instr.get_reg_op(0));
         let imm = self.i32_t.const_int(instr.get_imm_op(1) as u64, false);
-        self.last_instr = InstrHist {
-            opcode: instr.opcode,
-            inputs: vec![reg, imm],
-        };
+        self.set_last_instr(instr, vec![reg, imm]);
+        self.increment_pc(instr.mode);
     }
 
     /// TODO flags dependent on shift (is this encoded in cs instruction somehow?)
     /// TODO branch when mov'ing to PC
-    pub(super) fn arm_mov(&mut self, instr: &ArmDisasm) -> Result<()> {
-        let dest = instr.get_reg_op(0);
-        match instr
-            .operands
-            .get(1)
-            .ok_or(anyhow!("Missing 2nd operand"))?
-            .op_type
-        {
-            ArmOperandType::Reg(reg_id) => {
-                let src_reg = Reg::from(reg_id.0 as usize);
-                let src_val = self.reg_map.get(src_reg);
-                self.reg_map.update(dest, src_val);
+    pub(super) fn arm_mov(&mut self, instr: &ArmDisasm) {
+        let build = |f: &mut Self| -> Result<()> {
+            let dest = instr.get_reg_op(0);
+            match instr
+                .operands
+                .get(1)
+                .ok_or(anyhow!("Missing 2nd operand"))?
+                .op_type
+            {
+                ArmOperandType::Reg(reg_id) => {
+                    let src_reg = Reg::from(reg_id.0 as usize);
+                    let src_val = f.reg_map.get(src_reg);
+                    f.set_last_instr(instr, vec![f.reg_map.get(dest), src_val]);
+                    f.reg_map.update(dest, src_val);
+                }
+                ArmOperandType::Imm(imm) => {
+                    let imm_val = f.i32_t.const_int(imm as u64, false);
+                    f.set_last_instr(instr, vec![f.reg_map.get(dest), imm_val]);
+                    f.reg_map.update(dest, imm_val);
+                }
+                _ => panic!("unhandled op_type"),
             }
-            ArmOperandType::Imm(imm) => {
-                let imm_val = self.i32_t.const_int(imm as u64, false);
-                self.reg_map.update(dest, imm_val);
+            f.increment_pc(instr.mode);
+            Ok(())
+        };
+        self.exec_conditional(instr, build, false)
+    }
+
+    pub(super) fn arm_sub(&mut self, instr: &ArmDisasm) {
+        let build = |f: &mut Self| -> Result<()> {
+            let rd = instr.get_reg_op(0);
+            let rn = instr.get_reg_op(1);
+            let rn_val = f.reg_map.get(rn);
+            match instr
+                .operands
+                .get(2)
+                .ok_or(anyhow!("Missing 2nd operand"))?
+                .op_type
+            {
+                ArmOperandType::Reg(RegId(r)) => {
+                    let rm = Reg::from(r as usize);
+                    let rm_val = f.reg_map.get(rm);
+                    if instr.updates_flags {
+                        f.set_last_instr(instr, vec![rn_val, rm_val]);
+                    }
+                    let sub = f.builder.build_int_sub(rn_val, rm_val, "sub")?;
+                    f.reg_map.update(rd, sub);
+                }
+                ArmOperandType::Imm(imm) => {
+                    let imm_val = f.i32_t.const_int(imm as u64, false);
+                    if instr.updates_flags {
+                        f.set_last_instr(instr, vec![rn_val, imm_val]);
+                    }
+                    let sub = f.builder.build_int_sub(rn_val, imm_val, "sub")?;
+                    f.reg_map.update(rd, sub);
+                }
+                _ => panic!("unhandled op_type"),
             }
-            _ => panic!("unhandled op_type"),
-        }
-        Ok(())
+            Ok(())
+        };
+        self.exec_conditional(instr, build, false);
+    }
+
+    pub(super) fn arm_mul(&mut self, instr: &ArmDisasm) {
+        let build = |f: &mut Self| -> Result<()> {
+            let rd = instr.get_reg_op(0);
+            let rm = instr.get_reg_op(1);
+            let rs = instr.get_reg_op(2);
+            let rm_val = f.reg_map.get(rm);
+            let rs_val = f.reg_map.get(rs);
+            if instr.updates_flags {
+                f.set_last_instr(instr, vec![rm_val, rs_val]);
+            }
+            let res = f.builder.build_int_mul(rm_val, rs_val, "mul")?;
+            f.reg_map.update(rd, res);
+            Ok(())
+        };
+        self.exec_conditional(instr, build, false);
     }
 }
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn test_mov() {}
-}
+mod tests {}
