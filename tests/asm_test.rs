87ad0bbd68af233e6a5dc9d7b38afcdbdabc57dd
Roderick MacCrimmon <r.maccrimm@gmail.com>
2025-12-22 15:48:55 -0700

    Update stack and write locations to be valid writeable memory. End tests with infinite loop (to run on other emulators)

--- tests/asm_test.rs
+++ tests/asm_test.rs
@@ -5,6 +5,10 @@ use grimlet::emulator::{DebugOutput, Emulator};
 /// Labels produced by gvasm assume it's loaded into cartridge ROM
 const CART_START_ADDR: u32 = 0x08000000;
 
+const STACK_ADDR: u32 = 0x03008000;
+
+const EXIT_VAL: u32 = 0x6300;
+
 /// Simple framework for test cases written in assembly. Tests run assertions which, push 1 onto the
 /// stack (currently at 0x4000, end of the bios region), otherwise -1. The number of assertions made
 /// is written to register r8 and an exit is signalled to the emulator by writin 25344 to r11 and
@@ -17,13 +21,13 @@ macro_rules! assembly_test {
             let disasm = Disassembler::default();
             let mut emulator = Emulator::new(disasm);
             emulator.load_rom(path, CART_START_ADDR).unwrap();
-            let exit = |st: &ArmState| -> bool { st.regs[Reg::R11] == 25344 };
+            let exit = |st: &ArmState| -> bool { st.regs[Reg::R11] == EXIT_VAL };
             emulator.state.jump_to(CART_START_ADDR, ArmMode::ARM as i8);
             emulator.run(exit, Some(DebugOutput::Assembly));
             println!("{:08x?}", emulator.state.regs);
 
             let num_asserts = emulator.state.regs[Reg::R8];
-            let mut result_addr = 0x4000 - 4;
+            let mut result_addr = STACK_ADDR - 4;
             for i in 1..=num_asserts {
                 let word = emulator.state.mem.read::<u32>(result_addr);
                 println!("{}: {}", i, word);
@@ -31,7 +35,7 @@ macro_rules! assembly_test {
                 result_addr -= 4;
             }
             println!("{} assertions passed!", num_asserts);
-            assert_eq!(emulator.state.regs[Reg::SP], 0x4000 - (4 * num_asserts));
+            assert_eq!(emulator.state.regs[Reg::SP], STACK_ADDR - (4 * num_asserts));
         }
     };
 }
