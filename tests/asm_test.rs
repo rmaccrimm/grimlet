58a7def060c2e45882fa43c4ac42faf2320f465f
Roderick MacCrimmon <r.maccrimm@gmail.com>
2025-12-21 17:44:18 -0700

    Convert factorial test to assembly_test format

--- tests/asm_test.rs
+++ tests/asm_test.rs
@@ -5,90 +5,38 @@ use grimlet::emulator::{DebugOutput, Emulator};
 /// Labels produced by gvasm assume it's loaded into cartridge ROM
 const CART_START_ADDR: u32 = 0x08000000;
 
-#[test]
-fn test_factorial() {
-    let disasm = Disassembler::default();
-    let mut emulator = Emulator::new(disasm);
-    emulator
-        .load_rom("tests/programs/factorial.gba", CART_START_ADDR)
-        .unwrap();
+/// Simple framework for test cases written in assembly. Tests run assertions which, push 1 onto the
+/// stack (currently at 0x4000, end of the bios region), otherwise -1. The number of assertions made
+/// is written to register r8 and an exit is signalled to the emulator by writin 25344 to r11 and
+/// jumping to a no-op.
+macro_rules! assembly_test {
+    ($name:ident.gba) => {
+        #[test]
+        fn $name() {
+            let path = format!("tests/programs/{}.gba", stringify!($name));
+            let disasm = Disassembler::default();
+            let mut emulator = Emulator::new(disasm);
+            emulator.load_rom(path, CART_START_ADDR).unwrap();
+            let exit = |st: &ArmState| -> bool { st.regs[Reg::R11] == 25344 };
+            emulator.state.jump_to(CART_START_ADDR, false);
+            emulator.run(exit, Some(DebugOutput::Assembly));
+            println!("{:08x?}", emulator.state.regs);
 
-    let mut run = |n| -> u32 {
-        emulator.state.jump_to(CART_START_ADDR, false);
-        emulator.state.regs[Reg::R0] = n;
-        emulator.run(
-            |st: &ArmState| -> bool { st.curr_instr_addr() >= 40 + (CART_START_ADDR as usize) },
-            Some(DebugOutput::Assembly),
-        );
-        emulator.state.regs[Reg::R1]
+            let num_asserts = emulator.state.regs[Reg::R8];
+            let mut result_addr = 0x4000 - 4;
+            for i in 1..=num_asserts {
+                let word = emulator.state.mem.read::<u32>(result_addr);
+                println!("{}: {}", i, word);
+                assert_eq!(word, 1, "failed on assertion {}", i);
+                result_addr -= 4;
+            }
+            println!("{} assertions passed!", num_asserts);
+            assert_eq!(emulator.state.regs[Reg::SP], 0x4000 - (4 * num_asserts));
+        }
     };
-    assert_eq!(run(0), 1);
-    assert_eq!(run(1), 1);
-    assert_eq!(run(2), 2);
-    assert_eq!(run(3), 6);
-    assert_eq!(run(4), 24);
-    assert_eq!(run(5), 120);
-    assert_eq!(run(12), 479001600);
 }
 
-#[test]
-fn test_basic_load_store() {
-    let disasm = Disassembler::default();
-    let mut emulator = Emulator::new(disasm);
-    emulator
-        .load_rom("tests/programs/load_store.gba", CART_START_ADDR)
-        .unwrap();
-    let exit = |st: &ArmState| -> bool { st.regs[Reg::R11] == 25344 };
-    emulator.state.jump_to(CART_START_ADDR, false);
-    emulator.run(exit, Some(DebugOutput::Assembly));
-    println!("{:08x?}", emulator.state.regs);
-
-    let num_asserts = emulator.state.regs[Reg::R8];
-    let mut result_addr = 0x4000 - 4;
-    for i in 1..=num_asserts {
-        let word = emulator.state.mem.read::<u32>(result_addr);
-        println!("{}: {}", i, word);
-        assert_eq!(word, 1, "failed on assertion {}", i);
-        result_addr -= 4;
-    }
-    println!("{} assertions passed!", num_asserts);
-
-    assert_eq!(emulator.state.regs[Reg::SP], 0x4000 - (4 * num_asserts));
-}
-
-#[test]
-fn test_bx() {
-    let disasm = Disassembler::default();
-    let mut emulator = Emulator::new(disasm);
-    emulator
-        .load_rom("tests/programs/bx.gba", CART_START_ADDR)
-        .unwrap();
-    let exit = |st: &ArmState| -> bool { st.regs[Reg::R1] == 1 };
-    emulator.state.jump_to(CART_START_ADDR, false);
-    emulator.run(exit, Some(DebugOutput::Struct));
-}
-
-#[test]
-fn test_mov_pc() {
-    let disasm = Disassembler::default();
-    let mut emulator = Emulator::new(disasm);
-    emulator
-        .load_rom("tests/programs/mov_pc.gba", CART_START_ADDR)
-        .unwrap();
-    let exit = |st: &ArmState| -> bool { st.regs[Reg::R11] == 25344 };
-    emulator.state.jump_to(CART_START_ADDR, false);
-    emulator.run(exit, None);
-    println!("{:08x?}", emulator.state.regs);
-
-    let num_asserts = emulator.state.regs[Reg::R8];
-    let mut result_addr = 0x4000 - 4;
-    for i in 1..=num_asserts {
-        let word = emulator.state.mem.read::<u32>(result_addr);
-        println!("{}: {}", i, word);
-        assert_eq!(word, 1, "failed on assertion {}", i);
-        result_addr -= 4;
-    }
-    println!("{} assertions passed!", num_asserts);
-
-    assert_eq!(emulator.state.regs[Reg::SP], 0x4000 - (4 * num_asserts));
-}
+assembly_test!(load_store.gba);
+assembly_test!(bx.gba);
+assembly_test!(mov_pc.gba);
+assembly_test!(factorial.gba);
