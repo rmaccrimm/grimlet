c36f0058e02259f2d46f3da4f1faddda524a85b4
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-03-27 10:18:32 -0600

    Start defining types for rendering

---
+++ src/emulator/video.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+pub struct Sprite<'a> {
+    mem: &'a [u8],
+}
+
+enum ObjMode {}
+
+struct ObjShape {
+    width: usize,
+    height: usize,
+}
+
+enum ColorMode {}
+
+impl<'a> Sprite<'a> {
+    fn y(&self) -> u8 { todo!() }
+
+    fn rotate_scale(&self) -> bool { todo!() }
+
+    fn double_disable(&self) -> bool { todo!() }
+
+    fn obj_mode(&self) -> ObjMode { todo!() }
+
+    fn mosaic(&self) -> bool { todo!() }
+
+    fn color_mode(&self) -> ColorMode { todo!() }
+
+    fn shape(&self) -> ObjShape { todo!() }
+
+    fn x(&self) -> u16 { todo!() }
+
+    fn rotate_scale_group(&self) -> u8 { todo!() }
+
+    fn horizontal_flip(&self) -> bool { todo!() }
+
+    fn vertical_flip(&self) -> bool { todo!() }
+
+    fn obj_size(&self) -> u8 { todo!() }
+
+    fn character_name(&self) -> u16 { todo!() }
+
+    fn priority(&self) -> u8 { todo!() }
+
+    fn palette_number(&self) -> u8 { todo!() }
+}
