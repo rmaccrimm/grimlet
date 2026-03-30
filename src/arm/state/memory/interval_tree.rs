dde77bab0b7d0388093867095987071a7afa86d6
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-03-29 22:22:47 -0600

    Fix updating of grand-parent node after re-balancing

--- src/arm/state/memory/interval_tree.rs
+++ src/arm/state/memory/interval_tree.rs
@@ -4,6 +4,7 @@ use std::cmp::{Ordering, Reverse};
 // given address.
 #[derive(Debug, Default)]
 pub struct IntervalTree {
+    root: Option<usize>,
     nodes: Vec<Node>,
 }
 
@@ -21,11 +22,14 @@ struct Node {
 impl IntervalTree {
     /// Insert a new interval. Has no effect if tree already contains the interval.
     pub fn insert(&mut self, ival: (u32, u32)) {
-        if self.nodes.is_empty() {
-            self.nodes.push(Node::new(ival, None));
-            return;
+        let mut n = match self.root {
+            None => {
+                self.nodes.push(Node::new(ival, None));
+                self.root = Some(0);
+                return;
+            }
+            Some(r) => r,
         };
-        let mut n = 0;
         loop {
             if self.nodes[n] < ival {
                 match self.nodes[n].left {
@@ -57,10 +61,10 @@ impl IntervalTree {
 
     /// Return all intervals containing x
     pub fn search(&self, x: u32) -> Vec<(u32, u32)> {
-        if self.nodes.is_empty() {
-            return vec![];
-        }
-        let mut n = 0;
+        let mut n = match self.root {
+            None => return vec![],
+            Some(r) => r,
+        };
         loop {
             let node = &self.nodes[n];
 
@@ -99,9 +103,6 @@ impl IntervalTree {
     /// Re-trace the tree from child node `c` towards the root, performing re-balancing operations
     /// as needed.
     fn retrace(&mut self, mut c: usize) {
-        let mut n: Option<usize> = None;
-        let mut g: Option<usize> = None;
-
         while let Some(p) = self.nodes[c].parent {
             let is_right_child = match self.nodes[p].right {
                 Some(r) => c == r,
@@ -110,15 +111,17 @@ impl IntervalTree {
             let c_balance = self.nodes[c].balance;
             let p_balance = self.nodes[p].balance;
 
-            if is_right_child {
+            // new root of the sub-tree and grand-parent, updated at end of loop iteration
+            let (n, g): (usize, Option<usize>) = if is_right_child {
                 if self.nodes[p].balance > 0 {
                     // unbalanced to the right
-                    g = self.nodes[p].parent;
-                    if c_balance < 0 {
-                        n = Some(self.rotate_right_left(p, c));
+                    let g = self.nodes[p].parent;
+                    let n = if c_balance < 0 {
+                        self.rotate_right_left(p, c)
                     } else {
-                        n = Some(self.rotate_left(p, c));
-                    }
+                        self.rotate_left(p, c)
+                    };
+                    (n, g)
                 } else if p_balance < 0 {
                     self.nodes[p].balance = 0;
                     break;
@@ -130,12 +133,13 @@ impl IntervalTree {
             } else {
                 if p_balance < 0 {
                     // unbalanced to the left
-                    g = self.nodes[p].parent;
-                    if c_balance > 0 {
-                        n = Some(self.rotate_left_right(p, c));
+                    let g = self.nodes[p].parent;
+                    let n = if c_balance > 0 {
+                        self.rotate_left_right(p, c)
                     } else {
-                        n = Some(self.rotate_right(p, c));
-                    }
+                        self.rotate_right(p, c)
+                    };
+                    (n, g)
                 } else if p_balance > 0 {
                     self.nodes[p].balance = 0;
                     break;
@@ -144,21 +148,32 @@ impl IntervalTree {
                     c = p;
                     continue;
                 }
+            };
+
+            self.nodes[n].parent = g;
+            match g {
+                Some(gi) => {
+                    if self.nodes[gi].left == Some(c) {
+                        self.nodes[gi].left = Some(n);
+                    } else {
+                        self.nodes[gi].right = Some(n);
+                    }
+                }
+                None => {
+                    self.root = Some(n);
+                }
             }
+            break;
         }
-        println!("{:?}", n);
-        println!("{:?}", g);
     }
 
     // p_balance = +1, c_balance in (0, +1)
     fn rotate_left(&mut self, p: usize, c: usize) -> usize {
         let c_left_child = self.nodes[c].left;
-        let g = self.nodes[p].parent;
 
         self.nodes[p].right = c_left_child;
         self.nodes[c].left = Some(p);
         self.nodes[p].parent = Some(c);
-        self.nodes[c].parent = g;
         if let Some(n) = c_left_child {
             self.nodes[n].parent = Some(p);
         }
@@ -176,7 +191,6 @@ impl IntervalTree {
     fn rotate_right_left(&mut self, p: usize, c: usize) -> usize {
         // Guaranteed to not be None, by balance of c
         let y = self.nodes[c].left.unwrap();
-        let g = self.nodes[p].parent;
 
         // 1st right rotation
         let y_right_child = self.nodes[y].right;
@@ -193,7 +207,6 @@ impl IntervalTree {
         self.nodes[p].right = y_left_child;
         self.nodes[y].left = Some(p);
         self.nodes[p].parent = Some(y);
-        self.nodes[y].parent = g;
         if let Some(n) = y_left_child {
             self.nodes[n].parent = Some(p);
         }
@@ -215,12 +228,10 @@ impl IntervalTree {
 
     fn rotate_right(&mut self, p: usize, c: usize) -> usize {
         let c_right_child = self.nodes[c].right;
-        let g = self.nodes[p].parent;
 
         self.nodes[p].left = c_right_child;
         self.nodes[c].right = Some(p);
         self.nodes[p].parent = Some(c);
-        self.nodes[c].parent = g;
         if let Some(n) = c_right_child {
             self.nodes[n].parent = Some(p);
         }
@@ -237,9 +248,8 @@ impl IntervalTree {
     fn rotate_left_right(&mut self, p: usize, c: usize) -> usize {
         // Guaranteed to not be None, by balance of c
         let y = self.nodes[c].right.unwrap();
-        let g = self.nodes[p].parent;
 
-        // 1st right rotation
+        // 1st left rotation
         let y_left_child = self.nodes[y].left;
         self.nodes[c].right = y_left_child;
         self.nodes[c].parent = Some(y);
@@ -249,12 +259,11 @@ impl IntervalTree {
             self.nodes[n].parent = Some(c);
         }
 
-        // 2nd left rotation
+        // 2nd right rotation
         let y_right_child = self.nodes[y].right;
         self.nodes[p].left = y_right_child;
         self.nodes[y].right = Some(p);
         self.nodes[p].parent = Some(y);
-        self.nodes[y].parent = g;
         if let Some(n) = y_right_child {
             self.nodes[n].parent = Some(p);
         }
