7831b5d9dfa60811602116f4bf92cd8a77b2d9c6
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-04-09 11:41:28 -0600

    Add unit tests for Node methods

--- src/utils/interval_tree/node.rs
+++ src/utils/interval_tree/node.rs
@@ -34,6 +34,7 @@ pub enum BF {
 
 impl<T: IntervalItem> Node<T> {
     pub fn new(ival: (T, T)) -> Self {
+        debug_assert!(ival.0 <= ival.1, "interval must be sorted ascending");
         Self {
             center: ival.0.average_floor(&ival.1),
             sorted_by_first: vec![ival],
@@ -46,6 +47,7 @@ impl<T: IntervalItem> Node<T> {
     }
 
     pub fn add(&mut self, ival: (T, T)) {
+        debug_assert!(ival.0 <= ival.1, "interval must be sorted ascending");
         if !self.sorted_by_first.contains(&ival) {
             self.sorted_by_first.push(ival);
             self.sorted_by_first.sort_by_key(|i| i.0);
@@ -54,10 +56,10 @@ impl<T: IntervalItem> Node<T> {
         }
     }
 
-    pub fn remove(&mut self, ival: (T, T)) -> bool {
+    pub fn remove(&mut self, ival: (T, T)) {
+        debug_assert!(ival.0 <= ival.1, "interval must be sorted ascending");
         self.sorted_by_first.retain(|i| *i != ival);
         self.sorted_by_last.retain(|i| *i != ival);
-        self.sorted_by_first.is_empty()
     }
 
     pub fn remove_start_leq(&mut self, q: T) {
@@ -165,5 +167,192 @@ impl TryFrom<(i32, i32)> for BF {
             -2 => Ok(BF::Unbalanced(Direction::Left)),
             _ => Err("Heights differ by more than 2".into()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_valid_interval() {
+        let node = Node::new((10, 10));
+        assert_eq!(node.sorted_by_first, vec![(10, 10)]);
+        assert_eq!(node.sorted_by_last, vec![(10, 10)]);
+    }
+
+    #[test]
+    #[should_panic(expected = "interval must be sorted ascending")]
+    fn test_new_invalid_interval() { Node::new((1, 0)); }
+
+    #[test]
+    #[should_panic(expected = "interval must be sorted ascending")]
+    fn test_add_invalid_interval() {
+        let mut node = Node::new((10, 20));
+        node.add((3, -4));
+    }
+
+    #[test]
+    #[should_panic(expected = "interval must be sorted ascending")]
+    fn test_remove_invalid_interval() {
+        let mut node = Node::new((10, 10));
+        node.remove((1, 0));
+    }
+
+    #[test]
+    fn test_add_in_order() {
+        let mut node = Node::new((50, 60));
+        node.add((10, 20));
+        node.add((30, 40));
+        node.add((70, 80));
+
+        assert_eq!(
+            node.sorted_by_first,
+            vec![(10, 20), (30, 40), (50, 60), (70, 80)]
+        );
+        assert_eq!(
+            node.sorted_by_last,
+            vec![(70, 80), (50, 60), (30, 40), (10, 20)]
+        );
+    }
+
+    #[test]
+    fn test_add_maintains_order() {
+        let mut node = Node::new((50, 50));
+        node.add((20, 90));
+        node.add((10, 50));
+        node.add((40, 70));
+        node.add((25, 400));
+        node.add((60, 60));
+
+        assert_eq!(
+            node.sorted_by_first,
+            vec![(10, 50), (20, 90), (25, 400), (40, 70), (50, 50), (60, 60)]
+        );
+        assert_eq!(
+            node.sorted_by_last,
+            // By insert order when there's a tie?
+            vec![(25, 400), (20, 90), (40, 70), (60, 60), (50, 50), (10, 50)]
+        );
+    }
+
+    #[test]
+    fn test_add_duplicate_not_added() {
+        let mut node = Node::new((10, 20));
+        node.add((10, 20));
+        node.add((10, 20));
+
+        assert_eq!(node.sorted_by_first, vec![(10, 20)]);
+        assert_eq!(node.sorted_by_last, vec![(10, 20)]);
+    }
+
+    #[test]
+    fn test_remove_only_item() {
+        let mut node = Node::new((10, 20));
+        node.remove((10, 20));
+        assert_eq!(node.sorted_by_first, vec![]);
+        assert_eq!(node.sorted_by_last, vec![]);
+    }
+
+    #[test]
+    fn test_remove_maintains_sort() {
+        let mut node = Node::new((10, 20));
+        node.add((20, 90));
+        node.add((30, 40));
+        node.add((0, 45));
+        node.add((40, 50));
+
+        node.remove((20, 30));
+        node.remove((40, 50));
+
+        assert_eq!(
+            node.sorted_by_first,
+            vec![(0, 45), (10, 20), (20, 90), (30, 40)]
+        );
+        assert_eq!(
+            node.sorted_by_last,
+            vec![(20, 90), (0, 45), (30, 40), (10, 20)]
+        );
+    }
+
+    #[test]
+    fn test_remove_nonexistent() {
+        let mut node = Node::new((10, 20));
+        node.add((30, 40));
+        node.remove((50, 60));
+        assert_eq!(node.sorted_by_first, vec![(10, 20), (30, 40)]);
+        assert_eq!(node.sorted_by_last, vec![(30, 40), (10, 20)]);
+    }
+
+    #[test]
+    fn test_remove_start_leq_maintains_sort() {
+        let mut node = Node::new((10, 20));
+        node.add((20, 100));
+        node.add((30, 40));
+        node.add((50, 60));
+        node.add((35, 40));
+        node.add((36, 70));
+        node.remove_start_leq(35);
+        assert_eq!(node.sorted_by_first, vec![(36, 70), (50, 60)]);
+        assert_eq!(node.sorted_by_last, vec![(36, 70), (50, 60)]);
+    }
+
+    #[test]
+    fn test_remove_start_leq_none() {
+        let mut node = Node::new((10, 20));
+        node.add((30, 40));
+        node.add((50, 60));
+        node.remove_start_leq(0);
+        assert_eq!(node.sorted_by_first, vec![(10, 20), (30, 40), (50, 60)]);
+    }
+
+    #[test]
+    fn test_remove_start_leq_all() {
+        let mut node = Node::new((10, 20));
+        node.add((30, 40));
+        node.add((100, 100));
+        node.remove_start_leq(100);
+        assert!(node.is_empty());
+        assert_eq!(node.sorted_by_first, vec![]);
+        assert_eq!(node.sorted_by_last, vec![]);
+    }
+
+    #[test]
+    fn test_remove_end_geq_maintains_sort() {
+        let mut node = Node::new((10, 20));
+        node.add((20, 100));
+        node.add((30, 40));
+        node.add((50, 60));
+        node.add((35, 69));
+        node.add((36, 70));
+        node.remove_end_geq(70);
+        assert_eq!(
+            node.sorted_by_first,
+            vec![(10, 20), (30, 40), (35, 69), (50, 60)]
+        );
+        assert_eq!(
+            node.sorted_by_last,
+            vec![(35, 69), (50, 60), (30, 40), (10, 20)]
+        );
+    }
+
+    #[test]
+    fn test_remove_end_geq_none() {
+        let mut node = Node::new((10, 20));
+        node.add((30, 40));
+        node.add((50, 60));
+        node.remove_end_geq(61);
+        assert_eq!(node.sorted_by_first, vec![(10, 20), (30, 40), (50, 60)]);
+    }
+
+    #[test]
+    fn test_remove_end_geq_all() {
+        let mut node = Node::new((10, 20));
+        node.add((30, 40));
+        node.add((100, 100));
+        node.remove_end_geq(0);
+        assert!(node.is_empty());
+        assert_eq!(node.sorted_by_first, vec![]);
+        assert_eq!(node.sorted_by_last, vec![]);
     }
 }
