d04c6d9fdb01f2471d4850803084945d7db0ba8e
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-04-08 20:57:04 -0600

    Move the interval_tree integration test to a binary for easier profiling

---
+++ src/bin/benchmark_interval_tree.rs
@@ -0,0 +1,75 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use std::time::Instant;
+
+use anyhow::Result;
+use clap::Parser;
+use grimlet::utils::interval_tree::IntervalTree;
+
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+#[derive(Parser)]
+struct Args {
+    path: PathBuf,
+}
+
+#[derive(Debug)]
+enum Op {
+    Insert(i32, i32),
+    Delete(i32, i32),
+}
+
+// Reference, 2026-04-08
+// Without dheap: 1mil ins -> .5mil del -> 1mil ins -> .5mil del
+// Completed in 24.20s
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let f = File::open(&args.path)?;
+    let reader = BufReader::new(f);
+
+    let mut ops = vec![];
+    for line in reader.lines() {
+        let l = line?;
+        let mut sp = l.split(" ");
+        let op = sp.next().expect("no op field");
+        let start: i32 = sp
+            .next()
+            .expect("no start field")
+            .parse()
+            .expect("parsing start failed");
+        let end: i32 = sp
+            .next()
+            .expect("no end field")
+            .parse()
+            .expect("parsing end failed");
+        if op == "i" {
+            ops.push(Op::Insert(start, end));
+        } else {
+            ops.push(Op::Delete(start, end));
+        }
+    }
+
+    println!("Performing {} operations...", ops.len());
+
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    let mut t = IntervalTree::<i32>::default();
+    let now = Instant::now();
+    for op in ops {
+        match op {
+            Op::Insert(s, e) => t.insert((s, e)),
+            Op::Delete(s, e) => {
+                t.remove((s, e));
+            }
+        }
+    }
+    let elapsed = now.elapsed();
+    println!("Completed in {:.2?}", elapsed);
+    t.verify();
+    println!("{} nodes in tree", t.len());
+    Ok(())
+}
