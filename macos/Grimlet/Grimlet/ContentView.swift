a0a8a94f3334b476695b75e131ed76efa0386ce4
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-04-17 01:21:20 -0600

    Add Hello World SwiftUI project

---
+++ macos/Grimlet/Grimlet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grimlet
+//
+//  Created by Roddy MacCrimmon on 2026-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos/Grimlet/Grimlet/GrimletApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrimletApp.swift
+//  Grimlet
+//
+//  Created by Roddy MacCrimmon on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct GrimletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
